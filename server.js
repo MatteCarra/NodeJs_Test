59469eebf5af67df9484d301073c99aa04060c69
MatteCarra <MatteCarra@users.noreply.github.com>
2017-04-13 09:38:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 const express = require('express');
 const app = new express();
-var x = 0;
 
 app.listen(8000, () =>{
 	console.log("Server in ascolto sulla porta 8000");
@@ -12,9 +11,9 @@ app.get('/', (req,res) => {
 });
 
 app.get('/Author', (req,res) => {
-	res.send('Site created by Zanni');
+	res.send('Site created by Carra');
 });
 
 app.get('*', (req,res) => {
-	res.status(404).send('Hic sunt leones! (404)');
+	res.status(404).send('404');
 });
